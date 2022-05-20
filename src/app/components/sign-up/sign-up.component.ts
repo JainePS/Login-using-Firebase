61b5fe0c1bec3c18b2e9e2e518197ec2614d8ca0
Jaine PS <jaine.aps@hotmail.com>
2022-05-20 02:40:33 -0300

    formulários

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -3,7 +3,7 @@ import { AuthService } from "../../shared/services/auth.service";
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+  styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent implements OnInit {
   constructor(
--- src/app/material/material-modules/material-modules.module.ts
+++ src/app/material/material-modules/material-modules.module.ts
@@ -7,6 +7,9 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [],
@@ -18,6 +21,9 @@ import { MatListModule } from '@angular/material/list';
     MatIconModule,
     MatDividerModule,
     MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ],
   exports: [
     MatSidenavModule,
@@ -26,6 +32,9 @@ import { MatListModule } from '@angular/material/list';
     MatIconModule,
     MatDividerModule,
     MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ],
 })
 export class MaterialModule {}
